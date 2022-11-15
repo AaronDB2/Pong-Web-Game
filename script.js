4b17aa0fdc3026dbabff07615daaaf12e25666bc
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-15 21:52:10 +0100

    Made the game playable

--- script.js
+++ script.js
@@ -91,9 +91,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -209,7 +206,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
-  
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -226,7 +223,6 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener('mousemove', (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -242,4 +238,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
